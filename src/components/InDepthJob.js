b4e0e1193f1a2fdef8f716bb219051a58105ec0f
Rylan Cole <rylancole@w134-87-141-133.wireless.uvic.ca>
2021-09-15 10:15:13 -0700

    Add 'Under Construction'

--- src/components/InDepthJob.js
+++ src/components/InDepthJob.js
@@ -1,5 +1,20 @@
+import { Box, Typography } from "@material-ui/core";
+import BuildIcon from "@material-ui/icons/Build";
+
 const InDepthJob = ({ data }) => {
-  return <p>{data}</p>;
+  return (
+    <Box
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "16px",
+      }}
+    >
+      <BuildIcon />
+      <Typography>Under Construction</Typography>
+    </Box>
+  );
 };
 
 export default InDepthJob;
